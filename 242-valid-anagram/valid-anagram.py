edd9d4463be331fa6efe248aa33c9a55ff04d5ee
shatakshi-3 <133852643+shatakshi-3@users.noreply.github.com>
2026-01-25 15:37:46 +0530

    Time: 15 ms (42.66%) | Memory: 19.6 MB (9.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,5 +2,23 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s=s.replace(" ","").lower()
         t=t.replace(" ","").lower()
-        return sorted(s) == sorted(t)
+        if len(s)!=len(t):
+            return False
+        count={}
+
+        for i in s:
+            if i in count:
+                count[i]+=1
+            else:
+                count[i]=1
+
+        for i in t:
+            if i in count:
+                count[i]-=1
+                if count[i]<0:
+                    return False
+            else:
+                return False
+        return True
+
         
